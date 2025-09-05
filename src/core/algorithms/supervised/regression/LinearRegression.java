f80855da0954a48461e582c1b120d25521ecfb16
Devanshi-cmd <devanshitiwari250@gmail.com>
2025-09-06 04:05:14 +0530

    Add Linear Regression Model

---
+++ src/core/algorithms/supervised/regression/LinearRegression.java
@@ -0,0 +1,77 @@
+package core.algorithms.supervised.regression;
+
+public class LinearRegression {
+    double learning_rate;
+    int epochs;
+    double weights[];
+    double bias;
+
+    public LinearRegression(double learning_rate,int epochs,int n){
+        this.learning_rate=learning_rate;
+        this.epochs=epochs;
+        this.weights=new double[n];
+        // n-> no of features
+        this.bias=0.0;
+    }
+
+    //user does model.fit(int[][] dataset,int[] output)
+
+    public double predict_one_row(double[] row_data){
+        double ans=0.0;
+        for(int i=0;i<weights.length;i++){
+            ans+=weights[i]*row_data[i];
+            //just like y=mx+c
+        }
+        ans=ans+bias;
+        return ans;
+    }
+
+    public double[] predict_all(double[][] input_data){
+        double[] prediction=new double[input_data.length]; //will contain the predictions made for each row 
+        for(int i=0;i<input_data.length;i++){
+            prediction[i]=predict_one_row(input_data[i]);
+        }
+        return prediction;
+    }
+
+    public double error_cal(double[][]input_data,double[] output_data){
+        //error- mean square error
+        double error=0;
+        int n=output_data.length;
+        for(int i=0;i<n;i++){
+            //firstly we will predict the values for i/p data
+            double predicted_ans= predict_one_row(input_data[i]);
+            error+=(predicted_ans-output_data[i])*(predicted_ans-output_data[i]);
+        }
+        return error/n;
+
+    }
+    public void modify(double[] x,double y){
+        //handling gradient descent
+        double y_predicted=predict_one_row(x);
+        double e=y-y_predicted;
+
+        for(int i=0;i<weights.length;i++){
+            weights[i]=weights[i]+(learning_rate*e*x[i]);
+        }
+        bias=bias+(learning_rate * e);
+    }
+
+    public void fit(double[][] input_data,double[] output_data){
+        for(int i=0;i<epochs;i++){
+            for(int j=0;j<input_data.length;j++){
+                //traversing every row
+                modify(input_data[j],output_data[j]);
+            }
+        }
+    }
+
+    public double[] getWeights() { 
+        return weights; 
+    }
+
+    public double getBias() { 
+        return bias;
+    }
+
+}
